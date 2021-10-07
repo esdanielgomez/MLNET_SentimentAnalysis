8008a8f9108f99f69d6774d2b9a349ccb47651a6
Daniel Gomez Jaramillo <daniel.gomez@studentpartner.com>
2021-10-06 23:58:25 -0500

    ML

---
+++ ViewModels/DefaultViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using DotVVM.Framework.ViewModel;
+using DotVVM.Framework.Hosting;
+
+namespace TextClassification.ViewModels
+{
+    public class DefaultViewModel : MasterPageViewModel
+    {
+		public string InputText { get; set; }
+		public string Result { get; set; } = null;
+		public decimal? Score { get; set; } = null;
+
+		public void Predict()
+		{
+			var sampleData = new MLModel.ModelInput()
+			{
+				Col0 = InputText,
+			};
+
+			//Load model and predict output
+			var output = MLModel.Predict(sampleData);
+
+			if ((int)output.Prediction == 0)
+			{
+				Result = "Negative.";
+			}
+			else
+			{
+				Result = "Positive.";
+			}
+
+			Score = decimal.Round((decimal)(output.Score[(int)output.Prediction] * 100), 2);
+		}
+	}
+}
